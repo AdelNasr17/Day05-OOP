a006ed2763f48ae1732e0b886c29f611eea98119
adelnasr17102002@outlook.com <adelnasr17102002@outlook.com>
2025-01-18 21:00:43 +0200

    First Project

---
+++ Assignment 02 OOP/Point3D.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_02_OOP
+{
+    internal class Point3D
+    {
+        #region Properties
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        #endregion
+
+        #region onstructors 
+
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Point3D(int x, int y) : this(x, y, 0)
+        {
+        }
+
+        public Point3D(int x) : this(x, 0, 0)
+        {
+
+        }
+
+
+        #endregion
+
+        public override string ToString()
+        {
+            return $" point Coordinates :({X}, {Y}, {Z})";
+        }
+
+        #region Methods 
+
+        public static bool operator ==(Point3D P1, Point3D P2)
+        {
+            if (P1 is null || P2 is null)
+                return false;
+            else
+                return P1.X == P2.X && P1.Y == P2.Y && P1.Z == P2.Z;
+        }
+
+        public static bool operator !=(Point3D P1, Point3D P2)
+        {
+            return !(P1 == P2);
+        }
+
+        #endregion
+
+    }
+}
--- Assignment 02 OOP/Program.cs
+++ Assignment 02 OOP/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-          
+
+            #region First Project 
+
+            #region 2.Override the ToString Function to produce this output:
+
+            //Point3D P = new Point3D(10,10, 10);
+            //Console.WriteLine(P.ToString());
+
+            #endregion
+
+            #region 3. Read from the User the Coordinates for 2 points P1, P2 (Check the input using try Pares, Parse, Convert).
+            //Point3D P1 = ReadPoint("Enter coordinates for Point 1 (x  y  z): ");
+            //Point3D P2 = ReadPoint("Enter coordinates for Point 2 (x  y  z): ");
+            //Console.WriteLine($" P1 = {P1}");
+            //Console.WriteLine($" P2 = {P2}");
+
+            #endregion
+
+            #endregion
+
+
+
+        }
+
+
+        static Point3D ReadPoint(string message)
+        {
+            Console.Write(message);
+            string? input = Console.ReadLine();
+            string[] parts = input.Split(" ");
+
+            try
+            {
+                int X = int.Parse(parts[0]);
+                int Y = parts.Length > 1 ? int.Parse(parts[1]) : 0;
+                int Z = parts.Length > 2 ? int.Parse(parts[2]) : 0;
+
+                return new Point3D(X, Y, Z);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input format. Please enter integers.");
+                return ReadPoint(message);
+            }
         }
     }
 }
